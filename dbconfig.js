362f2592b866574e2807e76f63adf7ec43e6be34
fmontallana <fmontallana@integr8.com.ph>
2025-01-22 08:47:08 +0800

    initialize React and Vite project with basic configuration and structure

---
+++ dbconfig.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAGH1LaKCjYmeEYnfZdKqKr2cUSZpcYYjA",
+  authDomain: "blvckbox-backend.firebaseapp.com",
+  projectId: "blvckbox-backend",
+  storageBucket: "blvckbox-backend.firebasestorage.app",
+  messagingSenderId: "988077506538",
+  appId: "1:988077506538:web:1bc0cf38723865bda9bc93",
+  measurementId: "G-G2FRM75LSH",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const auth = getAuth(app);
---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { auth } from "../dbconfig";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+function App() {
+  const provider = new GoogleAuthProvider();
+  const [count, setCount] = useState(0);
+
+  const handleSignUp = async () => {
+    try {
+      const userCredential = await signInWithPopup(auth, provider);
+
+      console.log("User signed up:", userCredential.user);
+    } catch (error) {
+      console.error("Error signing up:", error.message);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={handleSignUp}>count is {count}</button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
